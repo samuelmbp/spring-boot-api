0c4479425aeb36d70c4b067971d96fee96a5c617
Samuel Raducan <samuel.thembp@gmail.com>
2022-10-08 15:04:50 +0100

    Create StudentRepository interface as a data access layer

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -8,16 +9,14 @@ import java.util.List;
 
 @Service
 public class StudentService {
+    private final StudentRepository studentRepository;
 
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Autowired
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Jake",
-                        "jake@mail.com",
-                        LocalDate.of(2000, Month.APRIL, 7),
-                        21
-                )
-        );
+       return  studentRepository.findAll();
     }
 }
