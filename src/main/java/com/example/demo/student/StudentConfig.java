8e489cf9ebad0d61ebc63d1a1aa69d92f3cd10e9
Samuel Raducan <samuel.thembp@gmail.com>
2022-10-08 16:08:53 +0100

    Create the StudentConfig file to save multiple students

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -1,2 +1,38 @@
-package com.example.demo.student;public class StudentConfig {
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student jake = new Student(
+                    1L,
+                    "Jake",
+                    "jake@mail.com",
+                    LocalDate.of(2000, Month.APRIL, 7),
+                    22
+            );
+
+            Student herms = new Student(
+                    "Herms",
+                    "herms@mail.com",
+                    LocalDate.of(2001, Month.SEPTEMBER, 6),
+                    21
+            );
+
+            repository.saveAll(
+                    List.of(jake, herms)
+            );
+        };
+
+    }
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,8 +3,6 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Service
